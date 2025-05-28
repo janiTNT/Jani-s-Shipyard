6093d3efd911e4e0c5ff68cfd69e7ea17a015413
janiTNT <66884080+janiTNT@users.noreply.github.com>
2025-05-27 22:39:59 -0300

    stuffing

--- Hyperoid/Mineables/2.d
+++ Hyperoid/Mineables/2.d
@@ -4,38 +4,10 @@ a
 	&<../../Tutorials/Inserters/T/T2.rules>/Part
 	&<../../Tutorials/Inserters/T/T3.rules>/Part
 	&<../../Tutorials/Inserters/T/T3B.rules>/Part
+	&<../../Tutorials/Inserters/T/TXL.rules>/Part
 ]
 d
 [
-	//External Walls Clone
-	{
-		Key = "sw"// Ship bucket layer.
-		Value // The actual material.
-		{
-			UniqueBucket = 5
-			RenderStage = Low
-			IsRoof = false
-			DrawWithShipGhost = true
-			Inflate = .0001
-			HasNormals = true
-			AtlasTextureParams
-			{
-				MipLevels = 7 // Assumes 64x64 min part sprite size.
-			}
-			Material
-			{
-				Shader = "./data/ships/common/walls_external_lit.shader"
-			}
-			DiffuseMaterial
-			{
-				Shader = "./data/ships/common/walls_external.shader"
-			}
-			NormalsMaterial
-			{
-				Shader = "./data/ships/common/walls_external_normals.shader"
-			}
-		}
-	}
 	//Roof Clone
 	{
 		Key = "sr"// Ship bucket layer.
